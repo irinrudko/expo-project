91de28fc94ed3448733bf354648812ebe5cb840d
Iryna Rudzko <irinrudko@gmail.com>
2023-01-09 14:13:46 +0400

    feat: add Global styles

---
+++ GlobalStyles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet, TextInput, View, Text } from 'react-native'
+
+export const GlobalStyles = StyleSheet.create({
+    border: {
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: 'black',
+    },
+})
